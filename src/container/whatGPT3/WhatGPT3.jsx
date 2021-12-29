e7a6d33f42880213d67367e668b686a6bffd44a8
Mohemmad Imtiyaz <imtiyajkhan789@gmail.com>
2021-12-29 11:57:32 +0530

    added brand and design it

--- src/container/whatGPT3/WhatGPT3.jsx
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -1,42 +1,16 @@
-// import React from 'react'
-// import './whatGPT3.css'
-// const WhatGPT3 = () => {
-//     return (
-//         <div>
-//             <h1>whatGPT3</h1>
-//         </div>
-//     )
-// }
+import React from 'react'
+import { Feature } from '../../Components'
+import './whatGPT3.css'
 
-// export default WhatGPT3
-
-import React from "react";
-
-export class WhatGPT3 extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: 0
-    };
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextState.value > 5) return false;
-    return true;
-  }
-  componentDidUpdate() {
-    console.log("->");
-  }
-  componentDidMount() {
-    setInterval(() => {
-      this.setState((prevState) => {
-        return {
-          value: prevState.value + 1
-        };
-      });
-    }, 1000);
-  }
-  render() {
-    console.log(this.state.value);
-    return <div>{this.state.value}</div>;
-  }
+const WhatGPT3 = () => {
+    return (
+        <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+            <div className='gpt3__whatgpt3-feature'>
+                <Feature/>
+            </div>
+        </div>
+    )
 }
+
+export default WhatGPT3
+
